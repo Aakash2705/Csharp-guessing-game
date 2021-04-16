57064544e73068d616819ad06d518c302517b313
Aakash2705 <aakashbose2705@gmail.com>
2021-04-16 15:49:44 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Guessing_Game
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random number = new Random();
+
+            int rightNum = number.Next(0 , 5);
+
+            bool won = false;
+
+            do
+            {
+                try
+                {
+                    Console.Write("Guess a number between 0 and 5:");
+                    int guess = Convert.ToInt32(Console.ReadLine());
+
+                    if (guess == rightNum)
+                    {
+                        Console.WriteLine("Congrats! u guessed the num.");
+                        won = true;
+                        Console.ReadLine(); //Waits for input so that Terminal doesn't close 
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Enter Number urod.");
+                }
+            }while (!won);
+        }
+    }
+}
